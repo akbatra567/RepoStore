1f25ad1891512b04ba4c33a7e700fd88c44a3a7a
Akshit Batra <akbatra567@gmail.com>
2024-08-11 02:12:53 +0530

    chore(setup): Initial commit

---
+++ backend/src/main/java/com/buildingforfun/Repo/Store/RepoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.buildingforfun.Repo.Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoStoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/buildingforfun/Repo/Store/RepoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildingforfun.Repo.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
